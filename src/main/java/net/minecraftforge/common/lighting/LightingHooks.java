f8c476f564f4d898ec40772f0bc90c71dd5d9e14
PhiPro95 <PProvenzano@web.de>
2018-05-01 20:29:30 +0200

    "Cleanup" initial lighting code

--- src/main/java/net/minecraftforge/common/lighting/LightingHooks.java
+++ src/main/java/net/minecraftforge/common/lighting/LightingHooks.java
@@ -26,9 +26,11 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumFacing.Axis;
 import net.minecraft.util.EnumFacing.AxisDirection;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.PooledMutableBlockPos;
 import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import net.minecraftforge.fml.common.FMLLog;
 
@@ -38,6 +40,215 @@ public class LightingHooks
     private static final AxisDirection[] ENUM_AXIS_DIRECTION_VALUES = AxisDirection.values();
 
     public static final int FLAG_COUNT = 32; //2 light types * 4 directions * 2 halves * (inwards + outwards)
+
+    public static final int CHUNK_COORD_OVERFLOW_MASK = -1 << 4;
+
+    public static void onLoad(final World world, final Chunk chunk)
+    {
+        initChunkLighting(world, chunk);
+        initNeighborLight(world, chunk);
+        scheduleRelightChecksForChunkBoundaries(world, chunk);
+    }
+
+    public static void writeLightData(final Chunk chunk, final NBTTagCompound nbt)
+    {
+        writeNeighborInitsToNBT(chunk, nbt);
+        writeNeighborLightChecksToNBT(chunk, nbt);
+    }
+
+    public static void readLightData(final Chunk chunk, final NBTTagCompound nbt)
+    {
+        readNeighborInitsFromNBT(chunk, nbt);
+        readNeighborLightChecksFromNBT(chunk, nbt);
+    }
+
+    public static void fillSkylightColumn(final Chunk chunk, final int x, final int z)
+    {
+        final ExtendedBlockStorage[] extendedBlockStorage = chunk.getBlockStorageArray();
+
+        final int height = chunk.getHeightValue(x, z);
+
+        for (int j = height >> 4; j < extendedBlockStorage.length; ++j)
+        {
+            final ExtendedBlockStorage blockStorage = extendedBlockStorage[j];
+
+            if (blockStorage == Chunk.NULL_BLOCK_STORAGE)
+                continue;
+
+            final int yMin = Math.max(j << 4, height);
+
+            for (int y = yMin & 15; y < 16; ++y)
+                blockStorage.setSkyLight(x, y, z, EnumSkyBlock.SKY.defaultLightValue);
+        }
+
+        chunk.markDirty();
+    }
+
+    public static void initChunkLighting(final World world, final Chunk chunk)
+    {
+        if (chunk.isLightPopulated() || chunk.pendingNeighborLightInits != 0)
+            return;
+
+        chunk.pendingNeighborLightInits = 15;
+
+        chunk.markDirty();
+
+        final int xBase = chunk.x << 4;
+        final int zBase = chunk.z << 4;
+
+        final PooledMutableBlockPos pos = PooledMutableBlockPos.retain();
+
+        final ExtendedBlockStorage[] extendedBlockStorage = chunk.getBlockStorageArray();
+
+        for (int j = 0; j < extendedBlockStorage.length; ++j)
+        {
+            final ExtendedBlockStorage blockStorage = extendedBlockStorage[j];
+
+            if (blockStorage == Chunk.NULL_BLOCK_STORAGE)
+                continue;
+
+            for (int x = 0; x < 16; ++x)
+            {
+                for (int z = 0; z < 16; ++z)
+                {
+                    for (int y = 0; y < 16; ++y)
+                    {
+                        if (blockStorage.get(x, y, z).getLightValue(world, pos.setPos(xBase + x, (j << 4) + y, zBase + z)) > 0)
+                            world.checkLightFor(EnumSkyBlock.BLOCK, pos);
+                    }
+                }
+            }
+        }
+
+        pos.release();
+
+        if (!world.provider.hasSkyLight())
+            return;
+
+        for (int x = 0; x < 16; ++x)
+        {
+            for (int z = 0; z < 16; ++z)
+            {
+                final int yMax = chunk.getHeightValue(x, z);
+                int yMin = Math.max(yMax - 1, 0);
+
+                for (final EnumFacing dir : EnumFacing.HORIZONTALS)
+                {
+                    final int nX = x + dir.getFrontOffsetX();
+                    final int nZ = z + dir.getFrontOffsetZ();
+
+                    if (((nX | nZ) & CHUNK_COORD_OVERFLOW_MASK) != 0)
+                        continue;
+
+                    yMin = Math.min(yMin, chunk.getHeightValue(nX, nZ));
+                }
+
+                scheduleRelightChecksForColumn(world, EnumSkyBlock.SKY, xBase + x, zBase + z, yMin, yMax - 1);
+            }
+        }
+    }
+
+    private static void initNeighborLight(final World world, final Chunk chunk, final Chunk nChunk, final EnumFacing nDir)
+    {
+        final int flag = 1 << nDir.getHorizontalIndex();
+
+        if ((chunk.pendingNeighborLightInits & flag) == 0)
+            return;
+
+        chunk.pendingNeighborLightInits ^= flag;
+
+        if (chunk.pendingNeighborLightInits == 0)
+            chunk.setLightPopulated(true);
+
+        chunk.markDirty();
+
+        final int xOffset = nDir.getFrontOffsetX();
+        final int zOffset = nDir.getFrontOffsetZ();
+
+        final int xMin;
+        final int zMin;
+
+        if ((xOffset | zOffset) > 0)
+        {
+            xMin = 0;
+            zMin = 0;
+        }
+        else
+        {
+            xMin = 15 * (xOffset & 1);
+            zMin = 15 * (zOffset & 1);
+        }
+
+        final int xMax = xMin + 15 * (zOffset & 1);
+        final int zMax = zMin + 15 * (xOffset & 1);
+
+        final int xBase = nChunk.x << 4;
+        final int zBase = nChunk.z << 4;
+
+        final PooledMutableBlockPos pos = PooledMutableBlockPos.retain();
+
+        for (int x = xMin; x <= xMax; ++x)
+        {
+            for (int z = zMin; z <= zMax; ++z)
+            {
+                int yMin = chunk.getHeightValue((x - xOffset) & 15, (z - zOffset) & 15);
+
+                // Restore a value <= initial height
+                for (; yMin > 0; --yMin)
+                {
+                    if (chunk.getCachedLightFor(EnumSkyBlock.SKY, pos.setPos(xBase + x - xOffset, yMin - 1, zBase + z - zOffset)) < EnumSkyBlock.SKY.defaultLightValue)
+                        break;
+                }
+
+                int yMax = nChunk.getHeightValue(x, z) - 1;
+
+                for (final EnumFacing dir : EnumFacing.HORIZONTALS)
+                {
+                    final int nX = x + dir.getFrontOffsetX();
+                    final int nZ = z + dir.getFrontOffsetZ();
+
+                    if (((nX | nZ) & CHUNK_COORD_OVERFLOW_MASK) != 0)
+                        continue;
+
+                    yMax = Math.min(yMax, nChunk.getHeightValue(nX, nZ));
+                }
+
+                scheduleRelightChecksForColumn(world, EnumSkyBlock.SKY, xBase + x, zBase + z, yMin, yMax - 1);
+            }
+        }
+
+        pos.release();
+    }
+
+    public static void initNeighborLight(final World world, final Chunk chunk)
+    {
+        final IChunkProvider provider = world.getChunkProvider();
+
+        for (final EnumFacing dir : EnumFacing.HORIZONTALS)
+        {
+            final Chunk nChunk = provider.getLoadedChunk(chunk.x + dir.getFrontOffsetX(), chunk.z + dir.getFrontOffsetZ());
+
+            if (nChunk == null)
+                continue;
+
+            initNeighborLight(world, chunk, nChunk, dir);
+            initNeighborLight(world, nChunk, chunk, dir.getOpposite());
+        }
+    }
+
+    public static final String neighborLightInitsKey = "PendingNeighborLightInits";
+
+    private static void writeNeighborInitsToNBT(final Chunk chunk, final NBTTagCompound nbt)
+    {
+        if (chunk.pendingNeighborLightInits != 0)
+            nbt.setShort(neighborLightInitsKey, chunk.pendingNeighborLightInits);
+    }
+
+    private static void readNeighborInitsFromNBT(final Chunk chunk, final NBTTagCompound nbt)
+    {
+        if (nbt.hasKey(neighborLightInitsKey, 2))
+            chunk.pendingNeighborLightInits = nbt.getShort(neighborLightInitsKey);
+    }
 
     public static void initSkylightForSection(final World world, final Chunk chunk, final ExtendedBlockStorage section)
     {
@@ -311,7 +522,7 @@ public class LightingHooks
 
     public static final String neighborLightChecksKey = "NeighborLightChecks";
 
-    public static void writeNeighborLightChecksToNBT(final Chunk chunk, final NBTTagCompound nbt)
+    private static void writeNeighborLightChecksToNBT(final Chunk chunk, final NBTTagCompound nbt)
     {
         if (chunk.neighborLightChecks == null)
         {
@@ -337,7 +548,7 @@ public class LightingHooks
         }
     }
 
-    public static void readNeighborLightChecksFromNBT(final Chunk chunk, final NBTTagCompound nbt)
+    private static void readNeighborLightChecksFromNBT(final Chunk chunk, final NBTTagCompound nbt)
     {
         if (nbt.hasKey(neighborLightChecksKey, 9))
         {
